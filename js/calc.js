c41a50d8fec52b5ea66ad3d9f8a2964853e0b591
Leora Wenger <web@leoraw.com>
2018-12-17 09:44:45 -0500

    num - more than one digit and decimals

--- js/calc.js
+++ js/calc.js
@@ -1,7 +1,10 @@
 //alert('bob');
 
 const myButtons = document.querySelectorAll('span');
-const butt = document.querySelector(".calculator");
+//const butt = document.querySelector(".calculator");
+
+const screen = document.querySelector(".screen");
+let num = ""; //string until used
 
 const el = document.getElementById("calc");
 
@@ -15,6 +18,18 @@ for (let i = 0; i < myButtons.length; i++) {
     //console.log(myButtons[i].innerHTML);
     myButtons[i].addEventListener("click", function(){;
         console.log(myButtons[i].innerHTML);
+       // console.log(myButtons[i].className);
+
+        if(myButtons[i].className != "operator"){
+            screen.innerHTML = num + myButtons[i].innerHTML;
+            //num = parseInt(myButtons[i].innerHTML);
+            num = num + myButtons[i].innerHTML ;
+            //console.log(num);
+        }
+        if(myButtons[i].className === "clear"){
+            screen.innerHTML = "";
+            num = "";
+        }
     }, false);
   }
 
