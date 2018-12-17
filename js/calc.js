d7bf4d14dc19c817c5d7c315d2ece04a2fc68dd7
Leora Wenger <web@leoraw.com>
2018-12-17 13:14:41 -0500

    no divide by zero

--- js/calc.js
+++ js/calc.js
@@ -6,6 +6,7 @@ const myButtons = document.querySelectorAll('span');
 const screen = document.querySelector(".screen");
 var num = ""; //string until used
 var op = "";
+var result = 0;
 
 const el = document.getElementById("calc");
 
@@ -22,12 +23,12 @@ for (let i = 0; i < myButtons.length; i++) {
         //console.log(myButtons[i].innerHTML);
        // console.log(myButtons[i].className);
        if(myButtons[i].className === "clear"){
-            screen.innerHTML = "";
+            screen.innerHTML = 0;
             num = "";
             op = "";
          }
         else { 
-        if(myButtons[i].className != "operator"){
+        if((myButtons[i].className != "operator") && (myButtons[i].className != "equals")){
             
             if(op===""){
             screen.innerHTML = num + myButtons[i].innerHTML;
@@ -37,17 +38,22 @@ for (let i = 0; i < myButtons.length; i++) {
             }
             else {
                 //do the operation
-                let result = doOperation(myButtons[i].innerHTML, op);
+                result = doOperation(myButtons[i].innerHTML, op);
                 //let result = parseInt(num) + parseInt(myButtons[i].innerHTML);
-                //console.log(sum);
-                screen.innerHTML = result;
+                screen.innerHTML = myButtons[i].innerHTML;
             }
         }
-       
 
         if(myButtons[i].className === "operator"){
             op = doOperators(myButtons[i].innerHTML);
             }
+
+        if(myButtons[i].className === "equals"){
+                if(isFinite(result)){
+                  screen.innerHTML = result;
+                 }
+                else screen.innerHTML = "N/A";
+                }    
         }    
     }, false);
   }
@@ -81,18 +87,25 @@ function doOperators(myOp){
         switch(myOp){
             case '+':
                 answer = parseInt(num) + parseInt(myNum);
+                num = answer;
                 console.log(answer);
                 break;
             case '-':
                 answer = parseInt(num) - parseInt(myNum);
+                num = answer;
                 break;
             case '/':
                 answer = parseInt(num) / parseInt(myNum);
+                num = answer;
                 break;
             case '*':
                 answer = parseInt(num) * parseInt(myNum);
+                num = answer;
+                console.log(num);
                 break;
-            case 'C':
+            case '=':
+                break;    
+            case 'C': // this should not appear
                 answer = "bob";
                 break;    
         }   
