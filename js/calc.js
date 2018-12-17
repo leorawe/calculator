52b099418bbb97a64324ae4784261a13ccafdefc
Leora Wenger <web@leoraw.com>
2018-12-17 09:05:31 -0500

    playing with event listeners

--- js/calc.js
+++ js/calc.js
@@ -1,1 +1,21 @@
-alert('bob');
+//alert('bob');
+
+const myButtons = document.querySelectorAll('span');
+const butt = document.querySelector(".calculator");
+
+const el = document.getElementById("calc");
+
+// el.addEventListener("mouseover", function(e){
+//     console.log('wat', e);
+//     //alert('mousey');
+// });
+
+
+for (let i = 0; i < myButtons.length; i++) {
+    console.log(myButtons[i].innerHTML);
+    myButtons[i].addEventListener("click", function(event){;
+        console.log(event.target);
+    }, false);
+  }
+
+
